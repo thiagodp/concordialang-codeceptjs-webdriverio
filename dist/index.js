c594978004b9e6a50b71791b91729d3b4579fb7c
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2019-04-08 16:56:55 -0300

    Add index file

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./CodeceptJS"));
+__export(require("./CommandMapper"));
+__export(require("./Commands"));
+__export(require("./ConfigMaker"));
+__export(require("./ReportConverter"));
+__export(require("./TestScriptExecutor"));
+__export(require("./TestScriptGenerator"));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './CodeceptJS';
+export * from './CommandMapper';
+export * from './Commands';
+export * from './ConfigMaker';
+export * from './ReportConverter';
+export * from './TestScriptExecutor';
+export * from './TestScriptGenerator';
