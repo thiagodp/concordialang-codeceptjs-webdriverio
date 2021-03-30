796bcfde6ffe9e96d791c3dfbe54801ae0fc4762
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2021-03-30 13:08:48 -0300

    Update in-ci script

--- dist/in-ci.d.ts
+++ dist/in-ci.d.ts
@@ -1,2 +1,2 @@
 declare function is(it: any): boolean;
-declare function isInCI(): boolean;
+declare function isInCI(process: any): boolean;
--- dist/in-ci.js
+++ dist/in-ci.js
@@ -1,10 +1,10 @@
 function is(it) {
     return !!it && it !== '0' && it !== 'false';
 }
-function isInCI() {
-    return is(process.env['CI']);
+function isInCI(process) {
+    return process && is(process.env['CI']);
 }
 // Returns with an error when *not* in CI mode
-if (!isInCI()) {
+if (!isInCI(process)) {
     process.exit(1);
 }
